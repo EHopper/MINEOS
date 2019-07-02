0aba198cdda089866883c4c946dae384279088dc
EHopper <emily.hopper353@gmail.com>
2019-07-02 15:23:07 -0400

    Add LDLIBS=-lgfortran -lm to MAKE_frechet.mk to define libraries (gfortran and maths) that were causing errors from undefined functions.

--- src/mineos/MAKE_frechet.mk
+++ src/mineos/MAKE_frechet.mk
@@ -1,6 +1,7 @@
-#LFLAGS= -L$(MINEOSLIB) 
+#LFLAGS= -L$(MINEOSLIB)
 # FFLAGS=-ffixed-line-length-none
 FFLAGS=-w -O1 -ffixed-line-length-none -fno-range-check
+LDLIBS=-lgfortran -lm
 
 frechetcomp = draw_frechet_gv frechet frechet_gv frechet_cv draw_frechet_gv
 
@@ -17,7 +18,7 @@ frechet: $(MINEOSBIN)/frechet
 
 $(MINEOSBIN)/frechet: frechet.o
 	-rm $(MINEOSBIN)/frechet
-	gfortran $(FFLAGS) -o $(MINEOSBIN)/frechet frechet.o 
+	gfortran $(FFLAGS) -o $(MINEOSBIN)/frechet frechet.o
 
 frechet.o: frechet.f
 	gfortran $(FFLAGS) -c -o frechet.o frechet.f
@@ -28,7 +29,7 @@ frechet_gv: $(MINEOSBIN)/frechet_gv
 
 $(MINEOSBIN)/frechet_gv: frechet_gv.o
 	-rm $(MINEOSBIN)/frechet_gv
-	gfortran $(FFLAGS) -o $(MINEOSBIN)/frechet_gv frechet_gv.o 
+	gfortran $(FFLAGS) -o $(MINEOSBIN)/frechet_gv frechet_gv.o
 
 frechet_gv.o: frechet_gv.f
 	gfortran $(FFLAGS) -c -o frechet_gv.o frechet_gv.f
@@ -39,7 +40,7 @@ frechet_cv: $(MINEOSBIN)/frechet_cv
 
 $(MINEOSBIN)/frechet_cv: frechet_cv.o
 	-rm $(MINEOSBIN)/frechet_cv
-	gfortran $(FFLAGS) -o $(MINEOSBIN)/frechet_cv frechet_cv.o 
+	gfortran $(FFLAGS) -o $(MINEOSBIN)/frechet_cv frechet_cv.o
 
 frechet_cv.o: frechet_cv.f
 	gfortran $(FFLAGS) -c -o frechet_cv.o frechet_cv.f
@@ -50,11 +51,11 @@ draw_frechet_gv: $(MINEOSBIN)/draw_frechet_gv
 
 $(MINEOSBIN)/draw_frechet_gv: draw_frechet_gv.o
 	-rm $(MINEOSBIN)/draw_frechet_gv
-	gfortran $(FFLAGS) -o $(MINEOSBIN)/draw_frechet_gv draw_frechet_gv.o 
+	gfortran $(FFLAGS) -o $(MINEOSBIN)/draw_frechet_gv draw_frechet_gv.o
 
 draw_frechet_gv.o: draw_frechet_gv.f
 	gfortran $(FFLAGS) -c -o draw_frechet_gv.o draw_frechet_gv.f
-	
+
 #----------------------------------
-clean: 
-	-rm *.o 
+clean:
+	-rm *.o
